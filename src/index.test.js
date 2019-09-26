540915c7a74778293bee8877d0609b53edb64b4a
Luke Moorman <kluke.moorman@gmail.com>
2019-09-25 19:43:59 -0500

    Clean up and refactor tests

--- src/index.test.js
+++ src/index.test.js
@@ -1,18 +1,33 @@
-import { main, elementOrSelector } from './index';
+import { elementOrSelector } from './index';
 import { idHtml, classHtml } from '../__tests__/fixtures';
 
-test('expect a greeting', () => {
-  expect(main()).toBe('Hello, World!');
-});
+describe('elementOrSelector', () => {
+  describe('given an id', () => {
+    it('returns an element', () => {
+      document.body.innerHTML = idHtml;
 
-test('given an element', () => {
-  document.body.innerHTML = idHtml;
-  expect(elementOrSelector('test')).toBeInstanceOf(HTMLDivElement);
-  document.body.innerHTML = classHtml;
-  expect(elementOrSelector('.test')).toBeInstanceOf(HTMLDivElement);
-  var myElement = document.createElement('div');
-  myElement.id = 'myId';
-  document.body.appendChild(myElement);
-  const myElement = document.getElementById('myId');
-  expect(elementOrSelector(myElement)).toBeInstanceOf(HTMLDivElement);
+      expect(elementOrSelector('test')).toBeInstanceOf(HTMLDivElement);
+    });
+  });
+
+  describe('given a selelctor', () => {
+    it('returns an element', () => {
+      document.body.innerHTML = classHtml;
+
+      expect(elementOrSelector('.test')).toBeInstanceOf(HTMLDivElement);
+    });
+  });
+
+  describe('given an element', () => {
+    it('returns an element', () => {
+      document.body.innerHTML = '';
+
+      // Set up a page with an element
+      var myElement = document.createElement('div');
+      myElement.id = 'myId';
+      document.body.appendChild(myElement);
+
+      expect(elementOrSelector(document.getElementById('myId'))).toBeInstanceOf(HTMLDivElement);
+    });
+  });
 });
